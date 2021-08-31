76bacc69360ebf06d2b45ecefde152df762e0e45
xukang <biabiabia0629@outlook.com>
2021-08-31 10:19:00 +0800

    update buildSrc to ComposingBuild. see version_and_plugin

---
+++ version_and_plugin/src/main/kotlin/com/biabiabia/wanandroid/version_and_plugin/Versions.kt
@@ -0,0 +1,31 @@
+package com.biabiabia.wanandroid.version_and_plugin
+
+object Versions {
+    private const val compose_version = "1.0.0"
+
+    const val androidx_core_core_ktx = "1.6.0"
+
+    const val androidx_appcompat_appcompat = "1.3.0"
+
+    const val androidx_activity_activity_compose = "1.3.0"
+
+    const val androidx_compose_ui_ui = compose_version
+    const val androidx_compose_ui_ui_tooling = compose_version
+    const val androidx_compose_ui_ui_tooling_preview = compose_version
+
+    const val androidx_compose_ui_eui_tst_junit4 = compose_version
+    const val androidx_compose_ui_ui_test_junit4 = compose_version
+
+    const val androidx_compose_material_material = compose_version
+
+    const val androidx_lifecycle_lifecycle_runtime_ktx = "2.3.1"
+
+    const val androidx_test_ext_junit = "1.1.3"
+    const val androidx_test_espresso_espresso_core = "3.4.0"
+
+    const val com_google_android_material_material = "1.4.0"
+
+    const val junit_junit = "4.13.2"
+
+    const val com_squareup_retrofit2_retrofit = "2.9.0"
+}
