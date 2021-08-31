0ef8026aa8935f04819b08b168274f0021268c79
xukang <biabiabia0629@outlook.com>
2021-08-31 11:33:25 +0800

    Optimize the definition rules for remote dependencies and add comments

--- version_and_plugin/src/main/kotlin/com/biabiabia/wanandroid/version_and_plugin/Deps.kt
+++ version_and_plugin/src/main/kotlin/com/biabiabia/wanandroid/version_and_plugin/Deps.kt
@@ -3,6 +3,21 @@ package com.biabiabia.wanandroid.version_and_plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * @author xk
+ *
+ * 自定义依赖plugin，在bulid.gradle中添加此plugin
+ * 可以自定义plugin，也可以仅使用定义的依赖描述
+ * 所有的依赖声明都放在此类中，根据 公司名/组件名/用途 进行分类
+ * 如：lifecycle组件则创建object Lifecycle 类
+ * 如：com.google.xxx/com.squareup.xxx 创建 [Google]/[Squareup]
+ * object类 命名规则为首字母大写即可
+ * 依赖名称的变量命名规则为：全小写；将依赖名称copy过来，将所有的[.]或[:]改为下划线 [_]即可
+ * 如：[Deps.Lifecycle.androidx_lifecycle_lifecycle_viewmodel_ktx]
+ * 注意，需要在 [Versions] 类中创建对应的类
+ *
+ * */
+
 class Deps : Plugin<Project> {
 
     override fun apply(target: Project) {
@@ -10,58 +25,109 @@ class Deps : Plugin<Project> {
     }
 
     object AndroidX {
-
-        const val androidx_core_core_ktx =
-            "androidx.core:core-ktx:${Versions.androidx_core_core_ktx}"
-
         const val androidx_appcompat_appcompat =
-            "androidx.appcompat:appcompat:${Versions.androidx_appcompat_appcompat}"
+            "androidx.appcompat:appcompat:${Versions.AndroidX.androidx_appcompat_appcompat}"
 
         const val androidx_compose_ui_ui =
-            "androidx.compose.ui:ui:${Versions.androidx_compose_ui_ui}"
+            "androidx.compose.ui:ui:${Versions.AndroidX.androidx_compose_ui_ui}"
 
         const val androidx_compose_material_material =
-            "androidx.compose.material:material:${Versions.androidx_compose_material_material}"
+            "androidx.compose.material:material:${Versions.AndroidX.androidx_compose_material_material}"
 
         const val androidx_compose_ui_ui_tooling =
-            "androidx.compose.ui:ui-tooling:${Versions.androidx_compose_ui_ui_tooling}"
+            "androidx.compose.ui:ui-tooling:${Versions.AndroidX.androidx_compose_ui_ui_tooling}"
 
         const val androidx_compose_ui_ui_tooling_preview =
-            "androidx.compose.ui:ui-tooling-preview:${Versions.androidx_compose_ui_ui_tooling_preview}"
+            "androidx.compose.ui:ui-tooling-preview:${Versions.AndroidX.androidx_compose_ui_ui_tooling_preview}"
 
-        const val androidx_lifecycle_lifecycle_runtime_ktx =
-            "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.androidx_lifecycle_lifecycle_runtime_ktx}"
 
         const val androidx_activity_activity_compose =
-            "androidx.activity:activity-compose:${Versions.androidx_activity_activity_compose}"
+            "androidx.activity:activity-compose:${Versions.AndroidX.androidx_activity_activity_compose}"
 
-        const val androidx_test_ext_junit =
-            "androidx.test.ext:junit:${Versions.androidx_test_ext_junit}"
+    }
 
-        const val androidx_test_espresso_espresso_core =
-            "androidx.test.espresso:espresso-core:${Versions.androidx_test_espresso_espresso_core}"
+    /**
+     * 生命周期感知型组件可执行操作来响应另一个组件（如 Activity 和 Fragment）的生命周期状态的变化。这些组件有助于您写出更有条理且往往更精简的代码，这样的代码更易于维护。
+     */
+    object Lifecycle {
 
-        const val androidx_compose_ui_ui_test_junit4 =
-            "androidx.compose.ui:ui-test-junit4:${Versions.androidx_compose_ui_ui_test_junit4}"
+        /**
+         * ViewModel*/
+        const val androidx_lifecycle_lifecycle_viewmodel_ktx =
+            "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle_version}"
 
-        const val androidx_compose_ui_eui_tst_junit4 =
-            "androidx.compose.ui:eui-tst-junit4:${Versions.androidx_compose_ui_eui_tst_junit4}"
+        /**
+         * LiveData*/
+        const val androidx_lifecycle_lifecycle_livedata_ktx =
+            "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle_version}"
+
+        /**
+         * Lifecycles only (without ViewModel or LiveData)*/
+        const val androidx_lifecycle_lifecycle_runtime_ktx =
+            "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle_version}"
+
+        /**
+         * Saved state module for ViewModel*/
+        const val androidx_lifecycle_lifecycle_viewmodel_savedstate =
+            "androidx.lifecycle:lifecycle-viewmodel-savedstate:${Versions.lifecycle_version}"
+
+        /**
+         * Kapt Annotation processor */
+        const val androidx_lifecycle_lifecycle_compiler =
+            "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle_version}"
+
+        /**
+         * optional - helpers for implementing LifecycleOwner in a Service */
+        const val androidx_lifecycle_lifecycle_service =
+            "androidx.lifecycle:lifecycle-service:${Versions.lifecycle_version}"
+
+        /**
+         * optional - ProcessLifecycleOwner provides a lifecycle for the whole application process */
+        const val androidx_lifecycle_lifecycle_process =
+            "androidx.lifecycle:lifecycle-process:${Versions.lifecycle_version}"
+
+    }
+
+    /**
+     * Android 上的 Kotlin 协程 */
+    object Coroutines {
+        const val org_jetbrains_kotlinx_kotlinx_coroutines_android =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.Coroutines.org_jetbrains_kotlinx_kotlinx_coroutines_android}"
+    }
+
+    object Android_KTX {
+        const val androidx_core_core_ktx =
+            "androidx.core:core-ktx:${Versions.Android_KTX.androidx_core_core_ktx}"
     }
 
     object Google {
         const val com_google_android_material_material =
-            "com.google.android.material:material:${Versions.com_google_android_material_material}"
+            "com.google.android.material:material:${Versions.Google.com_google_android_material_material}"
 
     }
 
     object Junit {
-        const val junit_junit = "junit:junit:${Versions.junit_junit}"
+        const val junit_junit = "junit:junit:${Versions.Junit.junit_junit}"
     }
 
     object Squareup {
         const val com_squareup_retrofit2_retrofit =
-            "com.squareup.retrofit2:retrofit${Versions.com_squareup_retrofit2_retrofit}"
+            "com.squareup.retrofit2:retrofit${Versions.Squareup.com_squareup_retrofit2_retrofit}"
 
     }
 
+    object Test {
+        const val androidx_test_ext_junit =
+            "androidx.test.ext:junit:${Versions.Test.androidx_test_ext_junit}"
+
+        const val androidx_test_espresso_espresso_core =
+            "androidx.test.espresso:espresso-core:${Versions.Test.androidx_test_espresso_espresso_core}"
+
+        const val androidx_compose_ui_ui_test_junit4 =
+            "androidx.compose.ui:ui-test-junit4:${Versions.Test.androidx_compose_ui_ui_test_junit4}"
+
+        const val androidx_compose_ui_eui_tst_junit4 =
+            "androidx.compose.ui:eui-tst-junit4:${Versions.Test.androidx_compose_ui_eui_tst_junit4}"
+
+    }
 }
--- version_and_plugin/src/main/kotlin/com/biabiabia/wanandroid/version_and_plugin/Versions.kt
+++ version_and_plugin/src/main/kotlin/com/biabiabia/wanandroid/version_and_plugin/Versions.kt
@@ -1,31 +1,50 @@
 package com.biabiabia.wanandroid.version_and_plugin
 
+/**
+ * 此类配合[Deps]使用，负责定义依赖的版本号
+ * */
 object Versions {
     private const val compose_version = "1.0.0"
 
-    const val androidx_core_core_ktx = "1.6.0"
+    const val lifecycle_version = "2.3.1"
 
-    const val androidx_appcompat_appcompat = "1.3.0"
+    object AndroidX {
+        const val androidx_appcompat_appcompat = "1.3.0"
 
-    const val androidx_activity_activity_compose = "1.3.0"
+        const val androidx_activity_activity_compose = "1.3.0"
 
-    const val androidx_compose_ui_ui = compose_version
-    const val androidx_compose_ui_ui_tooling = compose_version
-    const val androidx_compose_ui_ui_tooling_preview = compose_version
+        const val androidx_compose_ui_ui = compose_version
+        const val androidx_compose_ui_ui_tooling = compose_version
+        const val androidx_compose_ui_ui_tooling_preview = compose_version
 
-    const val androidx_compose_ui_eui_tst_junit4 = compose_version
-    const val androidx_compose_ui_ui_test_junit4 = compose_version
+        const val androidx_compose_material_material = compose_version
+    }
 
-    const val androidx_compose_material_material = compose_version
+    object Android_KTX {
+        const val androidx_core_core_ktx = "1.6.0"
+    }
 
-    const val androidx_lifecycle_lifecycle_runtime_ktx = "2.3.1"
+    object Coroutines {
+        const val org_jetbrains_kotlinx_kotlinx_coroutines_android = "1.3.9"
+    }
 
-    const val androidx_test_ext_junit = "1.1.3"
-    const val androidx_test_espresso_espresso_core = "3.4.0"
+    object Google {
+        const val com_google_android_material_material = "1.4.0"
+    }
 
-    const val com_google_android_material_material = "1.4.0"
+    object Junit {
+        const val junit_junit = "4.13.2"
+    }
 
-    const val junit_junit = "4.13.2"
+    object Test {
+        const val androidx_test_ext_junit = "1.1.3"
+        const val androidx_test_espresso_espresso_core = "3.4.0"
 
-    const val com_squareup_retrofit2_retrofit = "2.9.0"
+        const val androidx_compose_ui_eui_tst_junit4 = compose_version
+        const val androidx_compose_ui_ui_test_junit4 = compose_version
+    }
+
+    object Squareup {
+        const val com_squareup_retrofit2_retrofit = "2.9.0"
+    }
 }
